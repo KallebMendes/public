73b944e8ce26848a7c0f236a99e68497260dd679
Seu zehzinho <110861621+KallebMendes@users.noreply.github.com>
2023-09-12 21:38:54 -0300

    0000

---
+++ site/html/js/orçamento.js
@@ -0,0 +1,8 @@
+let form = document.getElementById("registro-orc")
+let button = document.getElementById("btn-enviar")
+
+button.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("funcionou");
+});
+
